4f38678f7c4ee75b0c7b6cc8d1d6bf50b5d5468d
ianpatricck <ianpatrick499@gmail.com>
2024-10-22 17:59:49 -0300

    feat: changed to BrowserRoute configuration and wrapping with motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Menu from "./pages/Menu";
+import Contact from "./pages/Contact";
+import Enter from "./pages/Enter";
+import Navbar from "./components/Navbar/Navbar";
+import { AnimatePresence } from "framer-motion";
+
 export default function App() {
   return (
     <>
-      <h1>Burgers!</h1>
+      <Router>
+        <Navbar />
+        <AnimatePresence>
+          <Routes>
+            <Route path="/" element={<Menu />} />
+            <Route path="/enter" element={<Enter />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </AnimatePresence>
+      </Router>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import "./style.scss";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
