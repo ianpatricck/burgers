c0f50d009aee7987f6fca55993cd6aeecea66ddb
ianpatricck <ianpatrick499@gmail.com>
2024-10-23 20:46:36 -0300

    removed react-query providers

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/main.scss";
 import App from "./App";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const root = createRoot(document.getElementById("root")!);
-const queryClient = new QueryClient();
 
 root.render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 );
