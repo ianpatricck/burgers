06608366804f4b3e61fefa57ed0bedd66ffe4548
ianpatricck <ianpatrick499@gmail.com>
2024-10-28 17:14:00 -0300

    feat: added motion toggle animation at contents

--- src/layouts/Contents/Contents.tsx
+++ src/layouts/Contents/Contents.tsx
@@ -2,9 +2,11 @@ import { KeyboardArrowUp } from "@mui/icons-material";
 import { useEffect, useState } from "react";
 import { MenuDetails } from "./types/MenuDetails";
 import { menuDetails } from "../../services/menuServices";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Contents() {
   const [menuItems, setMenuItems] = useState<MenuDetails[]>([]);
+  const [itemsSectionVisible, setItemsSectionsVisible] = useState<string[]>([]);
 
   useEffect(() => {
     const { sections } = menuDetails;
@@ -19,6 +21,10 @@ export default function Contents() {
     });
 
     setMenuItems(details);
+
+    const items = details.map((item) => item.name.toLowerCase());
+
+    setItemsSectionsVisible(items);
   }, []);
 
   function selectTab(tab: string) {
@@ -29,6 +35,17 @@ export default function Contents() {
     });
 
     setMenuItems(cleanedNavbarItems);
+  }
+
+  function toggleItem(itemName: string) {
+    if (!itemsSectionVisible.includes(itemName)) {
+      setItemsSectionsVisible([...itemsSectionVisible, itemName]);
+      return;
+    }
+
+    setItemsSectionsVisible(
+      itemsSectionVisible.filter((item) => item !== itemName),
+    );
   }
 
   return (
@@ -58,36 +75,64 @@ export default function Contents() {
       <main className="contents_main">
         {menuItems.map((item, item_index) => (
           <section className="contents_main_items" key={item_index}>
-            <nav className="contents_main_items_nav">
+            <nav
+              className="contents_main_items_nav"
+              onClick={() => toggleItem(item.name.toLowerCase())}
+            >
               <h1 className="contents_main_items__title">{item.name}</h1>
               <KeyboardArrowUp className="contents_main_items__icon" />
             </nav>
 
-            {item.items.map((food, food_index) => (
-              <div className="contents_main_items_wrapper" key={food_index}>
-                <div className="contents_main_items__info">
-                  <h1 className="contents_main_items__name">{food.name}</h1>
-                  <p className="contents_main_items__description">
-                    {food.description
-                      ? food.description
-                      : "Sem descrição provida"}
-                  </p>
-                  <span className="contents_main_items__price">
-                    R${food.price}
-                  </span>
-                </div>
+            <AnimatePresence>
+              {item.items.map((food, food_index) => (
+                <motion.div
+                  className="contents_main_items_wrapper"
+                  key={food_index}
+                  variants={{
+                    open: {
+                      opacity: 1,
+                    },
+                    closed: {
+                      opacity: 0,
+                      marginTop: -40,
+                      display: "none"
+                    }
+                  }}
+                  initial={{ opacity: 1, y: 0 }}
+                  animate={itemsSectionVisible.includes(item.name.toLowerCase()) ? "open" : "closed"}
+                  transition={{ duration: .1 }}
+                  exit={{ opacity: 0 }}
+                >
+                  {1 == 1  && (
+                    <>
+                      <div className="contents_main_items__info">
+                        <h1 className="contents_main_items__name">
+                          {food.name}
+                        </h1>
+                        <p className="contents_main_items__description">
+                          {food.description
+                            ? food.description
+                            : "Sem descrição provida"}
+                        </p>
+                        <span className="contents_main_items__price">
+                          R${food.price}
+                        </span>
+                      </div>
 
-                <div className="contents_main_items__image-wrapper">
-                  {food.images ? (
-                    <img
-                      className="contents_main_items__image"
-                      src={food.images[0].image}
-                      alt="Hardcore"
-                    />
-                  ) : null}
-                </div>
-              </div>
-            ))}
+                      <div className="contents_main_items__image-wrapper">
+                        {food.images ? (
+                          <img
+                            className="contents_main_items__image"
+                            src={food.images[0].image}
+                            alt={food.name}
+                          />
+                        ) : null}
+                      </div>
+                    </>
+                  )}
+                </motion.div>
+              ))}
+            </AnimatePresence>
           </section>
         ))}
       </main>
