0aeeaebaa9bb3f62341331d363dde5e89984020b
ianpatricck <ianpatrick499@gmail.com>
2024-11-14 20:33:53 -0300

    test: SearchBar component should be rendered

---
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import SearchBar from "./SearchBar";
+
+describe("SearchBar", () => {
+  it("should render and verify if search input is defined", () => {
+    render(<SearchBar />);
+
+    const input = screen.getByPlaceholderText("Search menu items");
+
+    expect(input).toBeDefined();
+  });
+});
