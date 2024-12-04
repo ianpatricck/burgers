99835a8c800cc7ac2cd216754da6059f6510c1bf
ianpatricck <ianpatrick499@gmail.com>
2024-12-04 17:32:32 -0300

    test: redux provider was implemented

--- src/components/SearchBar/SearchBar.test.tsx
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -2,10 +2,16 @@ import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import SearchBar from ".";
+import { store } from "../../storage/app/store";
+import { Provider } from "react-redux";
 
 describe("SearchBar", () => {
   it("should render the SearchBar component", () => {
-    const { getByTestId } = render(<SearchBar />);
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <SearchBar />
+      </Provider>,
+    );
     expect(getByTestId("searchbar")).toBeInTheDocument();
   });
 });
